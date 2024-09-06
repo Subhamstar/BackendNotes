65adf73572ce9628dc9348b06cd49feaba947764
Subhamstar <123460311+Subhamstar@users.noreply.github.com>
2024-09-06 16:35:27 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
     try {
       dispatch(signoutStart())
 
-      const res = await axios.get("http://localhost:3000/api/auth/signout", {
+      const res = await axios.get("https://subhamnotesbackend.onrender.com/api/auth/signout", {
         withCredentials: true,
       })
 
