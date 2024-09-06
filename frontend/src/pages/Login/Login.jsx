66749b0401f0fef25767a2a36fb39abd29386974
Subhamstar <123460311+Subhamstar@users.noreply.github.com>
2024-09-06 16:39:30 +0530

    Update Login.jsx

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -42,7 +42,7 @@ const Login = () => {
       dispatch(signInStart())
 
       const res = await axios.post(
-        "http://localhost:3000/api/auth/signin",
+        "https://subhamnotesbackend.onrender.com/api/auth/signin",
         { email, password },
         { withCredentials: true }
       )
